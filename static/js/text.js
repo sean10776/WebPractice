907b8a0bcb3e12080c8f453a3417139e13533843
sean10776 <sean10776@users.noreply.github.com>
2021-02-02 02:21:44 +0800

    add card sytle

--- static/js/text.js
+++ static/js/text.js
@@ -13,21 +13,21 @@ function Area_resize() {
     if (upper === true) {
         news.addClass('display-5 featurette-heading')
         Area.addClass("display-5")
-        $("#Area #content").addClass("h3")
+        $("#Area a").addClass("h3")
     } else {
         news.addClass("h2")
         Area.addClass("h4")
-        $("#Area #content").removeAttr('class')
+        $("#Area a").removeClass('h3')
     }
 }
 
 let s4= " * 擁有醉人香檳風味的東方美人茶，又名膨風茶、白毫烏龍茶、椪風茶<br>"
     +   " * 功效： 東方美人茶含有大量的茶多酚，可以提高脂肪分解酶的作用，降低血液中的膽固醇含量<br>"
-    +   " * 香氣： 熟果香和蜂蜜香<br>"
-    +   " * 味道： 蜜糖甘甜<br>"
-    +   " 東方美人茶擁有獨特的熟果味和蜜香，此香氣來自於小綠葉蟬，被它叮咬過的茶樹會啟用防禦機制釋放出以茉莉酸，為基礎的一系列香味物質。"
-    +   " 故東方美人茶在生產過程絕不使用農藥，藉此吸引小綠葉蟬的叮咬，增加被害蟲咬的機會。有蟲害的東方美人茶才是最高級之極品，是真正的有機茶！生產較為不易，也更顯其珍貴。"
-    +   " 也因為他擁有濃濃的蜂蜜香熟果味，西方飲茶人士譽之為東方美人（Oriental Beauty）。"
+    // +   " * 香氣： 熟果香和蜂蜜香<br>"
+    // +   " * 味道： 蜜糖甘甜<br>"
+    // +   " 東方美人茶擁有獨特的熟果味和蜜香，此香氣來自於小綠葉蟬，被它叮咬過的茶樹會啟用防禦機制釋放出以茉莉酸，為基礎的一系列香味物質。"
+    // +   " 故東方美人茶在生產過程絕不使用農藥，藉此吸引小綠葉蟬的叮咬，增加被害蟲咬的機會。有蟲害的東方美人茶才是最高級之極品，是真正的有機茶！生產較為不易，也更顯其珍貴。"
+    // +   " 也因為他擁有濃濃的蜂蜜香熟果味，西方飲茶人士譽之為東方美人（Oriental Beauty）。"
 
 function text_init(){
     $("#tea_park span").html(s2)
@@ -39,25 +39,32 @@ function img_init(){
 }
 
 function news_init(a){
-    let title = ""
-    let content = ""
-    let img_src = ""
+    let title = []
+    let content = []
+    let img_src = []
+    let total = 0
     switch(a){
         case "Resume_news":
             title = s1
             break
         case "Tea_news":
-            title = s2
-            content = s4
-            img_src = "img/Tea_park.jpg"
+            total = 2
+            title = ["東方美人", "擁有好的茶"]
+            content = [s4, s4]
+            img_src = ["img/Tea_park.jpg", "img/Tea_park2.jpg"]
             break
         case "More_news":
             title = s3
             break
     }
-    $('#' + a + ' span').html(title)
-    $('#' + a + ' p').html(content)
-    $('#' + a + ' img').attr("src", img_src)
+    let item = $('#tid-0').clone().removeAttr('style')
+    for(var i = 0; i < total;i++){
+        item.removeAttr('id').attr('id', 'tid-'+ i).addClass('item').appendTo("#"+a)
+        $('#tid-' + i + ' .text-inherit').html(title[i])
+        $('#tid-' + i + ' p').html(content[i])
+        $('#tid-' + i + ' img').attr("src", img_src[i])
+        item = item.clone()
+    }
 }
 
 function News_showup(){
@@ -69,6 +76,7 @@ function News_showup(){
             $('#news').slideUp('slow')
             $('#news div').first().removeAttr('id')
         }else if($('#news div').first().attr('id') !== N_id ){             //已打開按不同個
+            $('#news div').remove('.item')
             $('#news').slideUp('slow')
             $('#news div').first().removeAttr('id').attr("id", N_id)
             news_init(N_id)
